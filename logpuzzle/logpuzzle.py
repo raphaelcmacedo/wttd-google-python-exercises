ffbfaa55257a60e540953ae9d413d0c91dd1f8b1
raphaelcmacedo <raphaelcmacedo@hotmail.com>
2016-08-27 18:59:02 -0300

    Exercise logpuzzle

--- logpuzzle/logpuzzle.py
+++ logpuzzle/logpuzzle.py
@@ -9,7 +9,7 @@
 import os
 import re
 import sys
-import urllib
+import urllib.request
 
 """Logpuzzle exercise
 Given an apache logfile, find the puzzle urls and download the images.
@@ -18,13 +18,32 @@ Here's what a puzzle url looks like:
 10.254.254.28 - - [06/Aug/2007:00:13:48 -0700] "GET /~foo/puzzle-bar-aaab.jpg HTTP/1.0" 302 528 "-" "Mozilla/5.0 (Windows; U; Windows NT 5.1; en-US; rv:1.8.1.6) Gecko/20070725 Firefox/2.0.0.6"
 """
 
+key_word = 'puzzle'
 
 def read_urls(filename):
     """Returns a list of the puzzle urls from the given log file,
     extracting the hostname from the filename itself.
     Screens out duplicate urls and returns the urls sorted into
     increasing order."""
-    # +++your code here+++
+
+    index = filename.index('_') + 1
+    host = filename[index:]
+    d = {}
+
+    f = open(filename)
+    for line in f:
+        match = re.search(r'"GET (\S+)', line)
+
+        if match:
+            path = match.group(1)
+
+            if key_word in path:
+                key = 'http://' + host + path
+                if key in d:
+                    d[key] += 1
+                else:
+                    d[key] = 1
+    return sorted(d.keys())
 
 
 def download_images(img_urls, dest_dir):
@@ -35,8 +54,23 @@ def download_images(img_urls, dest_dir):
     with an img tag to show each local image file.
     Creates the directory if necessary.
     """
-    # +++your code here+++
+    if not os.path.exists(dest_dir):
+        os.makedirs(dest_dir)
 
+    html = open(os.path.join(dest_dir, 'index.html'), 'w')
+    html.write('<html><body>\n')
+
+    i = 0
+    for img_url in img_urls:
+        local_name = 'img%d' % i
+        urllib.request.urlretrieve(img_url, os.path.join(dest_dir, local_name))
+
+        html.write('<img src="%s">' % (local_name,))
+        i += 1
+
+    html.write('\n</body></html>\n')
+
+    html.close()
 
 def main():
     args = sys.argv[1:]
